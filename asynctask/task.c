328746572fbcc57cedad72e3dd1e9b2491b07abd
wangweipeng <weipeng.wang@gllue.com>
2019-04-18 16:43:11 +0800

    update

--- asynctask/task.c
+++ asynctask/task.c
@@ -68,7 +68,6 @@ void start_loop()
                 current = i;
                 tasks[i].status = 1;
                 run_and_store(&(main_task.handler), tasks[i].handler._start_fun);
-                printf("%s", "hfe");
             }
         }
     }
