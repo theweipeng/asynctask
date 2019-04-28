29822e49dd0e8c5a58ea49536b24ee9908e97906
wangweipeng <weipeng.wang@gllue.com>
2019-04-28 12:32:55 +0800

    update

--- main.c
+++ main.c
@@ -5,51 +5,33 @@
 
 int fun3(u_int64_t a, u_int64_t b, u_int64_t c)
 {
-    printf("%s", "fun3 start\n");
-    int ha = a - b;
     task_yield();
-    printf("%s", "fun3 end\n");
-    printf("%d", a);
-    printf("%s", "\n");
-    printf("%d", b);
-    printf("%s", "\n");
-    printf("%d", c);
-    printf("%s", "\n");
-    printf("%d", ha);
-    printf("%s", "\n");
-    return 999;
+    return a + b + c;
 }
 
 
-int fun2()
+int fun2(u_int64_t s)
 {
-    printf("%s", "fun2 start\n");
     task_yield();
     u_int64_t a2[] = {222, 333 , 444};
     int a = task_await(task_run(4096, fun3, a2));
-    printf("%d", a);
-    printf("%s", "fun2 end\n");
-    return 19;
+    return a + s;
 }
 
 int fun1(u_int64_t s)
 {
-    printf("%s", "fun1 start\n");
     u_int64_t a2[] = {111};
     int a = task_await(task_run(4096, fun2, a2));
-    printf("%d", a + s);
-    printf("%s", "\n");
-    printf("%s", "fun1 end\n");
-    return 10086;
+    u_int64_t a3[] = {9999};
+    int v = task_await(task_run(4096, fun2, a3));
+    printf("result %d", a + v + s);
+    printf("\n");
+    return a + v + s;
 }
 
 int fun4(u_int64_t s)
 {
-
-    printf("%s", "fun4 start\n");
-    printf("%d", s);
-    printf("%s", "fun4 end\n");
-    return 10086;
+    return s;
 }
 
 
@@ -58,8 +40,10 @@ int main()
     init_loop();
     u_int64_t a[] = {1};
     task_run(4096, fun1, a);
-    task_run(4096, fun1, a);
-    task_run(4096, fun4, a);
+
+    u_int64_t b[] = {-22};
+    task_run(4096, fun1, b);
+    task_run(4096, fun4, b);
     start_loop();
     return 0;
 }
